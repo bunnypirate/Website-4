92d563a9516da7fde62fb3ae7acfc603eeff771a
bunnypirate <bunnypirateprincess>
2017-01-23 20:10:54 -0800

    1 commit

---
+++ WebSite-4/WebSite-4/Models/MeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebSite_4.Models
+{
+    // Models returned by MeController actions.
+    public class GetViewModel
+    {
+        public string Hometown { get; set; }
+    }
+}
